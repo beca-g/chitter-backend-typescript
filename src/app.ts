5960a1df1a39d9cc72ceb77c2a6431e085756b0d
Beca-g <rebekahgalliano@gmail.com>
2021-04-29 11:37:10 +0100

    Corrected lint offences

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
-// import 'dotenv/config';
-import express, { Application, Request, Response, NextFunction } from 'express';
 import createServer from './server';
 
 const startServer = () => {
     const app = createServer();
-    const port: number = 4000;
+    const port = 4000;
 
     app.listen(port, () => {
         console.log(`server running on port ${port}`);
