5e7a451008a0001bc9a6df557a7c4e1abb2912d0
Beca-g <rebekahgalliano@gmail.com>
2021-05-18 16:16:50 +0100

    Corrected lint offences

--- src/models/peep.ts
+++ src/models/peep.ts
@@ -5,19 +5,19 @@ export class Peep {
 
 	constructor(body: string, date: number = Date.now(), id: number = Date.now()) {
 		this.body = body;
-		this.date = new Date(date).toLocaleString()
+		this.date = new Date(date).toLocaleString();
 		this.id = id;
 	}
 
 	getBody(): string {
-		return this.body
+		return this.body;
 	}
 
 	getTimeStamp(): string {
-		return this.date
+		return this.date;
 	}
 
 	getId(): number {
-		return this.id
+		return this.id;
 	}
 }
