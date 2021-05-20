de61ce1d69f8b3daf30aef3f19794e8ca229322e
Beca-g <rebekahgalliano@gmail.com>
2021-05-20 10:19:56 +0100

    Adding Chitter test

--- tests/models/chitter.test.ts
+++ tests/models/chitter.test.ts
@@ -10,16 +10,23 @@ describe("Chitter", () => {
 
   describe("getAllPeeps", () => {
     it("should show all peeps", () => {
-      chitter.createPeep("Peep")
-      chitter.createPeep("Peeper")
-      expect(chitter.getAllPeeps().length).to.equal(2)
+      chitter.createPeep("Peep");
+      chitter.createPeep("Peeper");
+      expect(chitter.getAllPeeps().length).to.equal(2);
     })
   })
 
   describe("createPeep", () => {
     it("should create a new peep", () => {
-      chitter.createPeep("Hi Hi")
-      expect(chitter.getAllPeeps()[0].body).to.equal("Hi Hi")
+      chitter.createPeep("Hi Hi");
+      expect(chitter.getAllPeeps()[0].body).to.equal("Hi Hi");
+    })
+  })
+
+  describe("createUser", () => {
+    it("should create a user", () => {
+      chitter.createUser("beca", "hello@beca.com", "password123");
+      expect(chitter.users.length).to.equal(1); 
     })
   })
 })
