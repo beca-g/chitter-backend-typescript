02e2443739c82c70a1f62a04e2cc48e4ed66d5b2
Beca-g <rebekahgalliano@gmail.com>
2021-06-01 14:26:43 +0100

    Created chitterController test

---
+++ tests/controllers/chitter.test.ts
@@ -0,0 +1,10 @@
+import { ChitterController } from "../../src/controllers/chitter/chitter";
+import { expect } from "chai";
+
+describe("ChitterController", () => {
+  let chitterController: ChitterController;
+
+  beforeEach(() => {
+    chitterController = new ChitterController();
+  });
+});
