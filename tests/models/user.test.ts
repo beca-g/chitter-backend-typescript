0b855ef1d9574d285a29e95bcd4e8d1840d0502b
Beca-g <rebekahgalliano@gmail.com>
2021-06-01 14:11:52 +0100

    Corrected lint offences

--- tests/models/user.test.ts
+++ tests/models/user.test.ts
@@ -1,36 +1,36 @@
-import { User } from "../../src/models/user"
-import { expect } from "chai"
+import { User } from "../../src/models/user";
+import { expect } from "chai";
 
 describe("User", () => {
   let user: User;
   let ID: number;
 
   beforeEach(() => {
-    user = new User("Beca", "hello@beca.com", "password123")
-    ID = Date.now()
+    user = new User("Beca", "hello@beca.com", "password123");
+    ID = Date.now();
   })
 
   describe("getUsername", () => {
     it("should return a username", () => {
-      expect(user.getUsername()).to.equal("Beca")
+      expect(user.getUsername()).to.equal("Beca");
     })
   })
 
   describe("getEmail", () => {
     it("should return a users email address", () => {
-      expect(user.getEmail()).to.equal("hello@beca.com")
+      expect(user.getEmail()).to.equal("hello@beca.com");
     })
   })
 
   describe("getPassword", () => {
     it("should return a users password", () => {
-      expect(user.getPassword()).to.equal("password123")
+      expect(user.getPassword()).to.equal("password123");
     })
   })
 
   describe("getId", () => {
     it("should return a users IDs", () => {
-      expect(user.getId()).to.equal(ID)
+      expect(user.getId()).to.equal(ID);
     })
   })
 })
