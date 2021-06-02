eaecfed31e7a6904681d24f04cd12b712d92e8c3
Beca-g <rebekahgalliano@gmail.com>
2021-06-02 13:26:28 +0100

    Updated peeps routes

--- src/routes/chitter.ts
+++ src/routes/chitter.ts
@@ -8,12 +8,12 @@ router.use(express.urlencoded({ extended: false }))
 
 const chitterController = new ChitterController();
 
-router.get("/", (req: Request, res: Response) => {
-	return res.status(200).send(chitterController.read());
+router.get("/peeps", (req: Request, res: Response) => {
+	return res.status(200).send(chitterController.readPeeps());
 });
 
-router.post("/", (req: Request, res: Response) => {
-	console.log("POST ROUTE REACHED")
+router.post("/peeps", (req: Request, res: Response) => {
+	console.log("POST PEEP ROUTE REACHED")
 	const peep = req.body.body
 	console.log("body is", peep);
 	chitterController.createPeep(peep)
