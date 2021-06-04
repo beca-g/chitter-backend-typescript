387646c2cdf2668ae0e29db9a3fce8fe9d839fee
Beca-g <rebekahgalliano@gmail.com>
2021-06-04 14:27:22 +0100

    Added guard clause to ensure usernames are unique

--- src/models/chitter.ts
+++ src/models/chitter.ts
@@ -1,24 +1,38 @@
-import { Peep } from "./peep"
-import { User } from "./user"
+import { Peep } from "./peep";
+import { User } from "./user";
 
 export class Chitter {
-  peeps: Peep[]
-  users: User[]
+  peeps: Peep[];
+  users: User[];
 
   constructor() {
-    this.peeps = []
-    this.users = []
+    this.peeps = [];
+    this.users = [];
   }
   
   getAllPeeps(): Peep[] {
-    return this.peeps
+    return this.peeps;
+  }
+
+  getAllUsers(): User[] {
+    return this.users;
   }
 
   createPeep(body: string): number {
-    return this.peeps.push(new Peep(body))
+    return this.peeps.push(new Peep(body));
   }
 
-  createUser(username: string, email: string, password: string): number {
-    return this.users.push(new User(username, email, password))
+  createUser(username: string, email: string, password: string): any {
+    if(this.validateUsername(username) == true){
+      return "Username unavailable";
+    }
+    return this.users.push(new User(username, email, password));
+  }
+
+  validateUsername(username: string): boolean {
+    return this.users.some(function(user){
+      console.log(user.username, username)
+      return user.username == username;
+    });
   }
 }
--- tests/models/chitter.test.ts
+++ tests/models/chitter.test.ts
@@ -1,32 +1,37 @@
-import { Chitter } from "../../src/models/chitter"
-import { expect } from "chai"
+import { Chitter } from "../../src/models/chitter";
+import { expect } from "chai";
 
 describe("Chitter", () => {
   let chitter: Chitter;
 
   beforeEach(() => {
     chitter = new Chitter();
-  })
+  });
 
   describe("getAllPeeps", () => {
     it("should show all peeps", () => {
       chitter.createPeep("Peep");
       chitter.createPeep("Peeper");
       expect(chitter.getAllPeeps().length).to.equal(2);
-    })
-  })
+    });
+  });
 
   describe("createPeep", () => {
     it("should create a new peep", () => {
       chitter.createPeep("Hi Hi");
       expect(chitter.getAllPeeps()[0].body).to.equal("Hi Hi");
-    })
-  })
+    });
+  });
 
   describe("createUser", () => {
     it("should create a user", () => {
       chitter.createUser("beca", "hello@beca.com", "password123");
       expect(chitter.users.length).to.equal(1); 
-    })
-  })
-})
+    });
+
+    it("should throw an error if the username is already taken", () => {
+      chitter.createUser("beca", "hello@beca.com", "password123");
+      expect(chitter.createUser("beca", "hello@beca.com", "password123")).to.equal("Username unavailable");
+    });
+  });
+});
