c00a02506b79b1d782e3f5b3f8d8d8c85394f825
Beca-g <rebekahgalliano@gmail.com>
2021-06-04 14:26:56 +0100

    Added user routes to create and show users

--- src/routes/chitter.ts
+++ src/routes/chitter.ts
@@ -20,4 +20,18 @@ router.post("/peeps", (req: Request, res: Response) => {
 	res.send(req.body)
 });
 
+router.post("/users", (req: Request, res: Response) => {
+	console.log("POST REGISTER ROUTE REACHED")
+	const username = req.body.username
+	const email = req.body.email
+	const password = req.body.password
+	console.log("new user is", username);
+	chitterController.createUser(username, email, password)
+	res.send(req.body.username)
+});
+
+router.get("/users", (req: Request, res: Response) => {
+	return res.status(200).send(chitterController.readUsers());
+});
+
 export default router;
--- tests/routes/chitter.test.ts
+++ tests/routes/chitter.test.ts
@@ -3,12 +3,24 @@ import request from "supertest";
 import createServer from "../../src/server";
 const app = createServer();
 
-describe("Chitter route", () => {
-  it("should respond with 200", (done) => {
-    request(app).get("/chitter").expect(200, done)
-  })
+describe("Chitter routes", () => {
+  describe("#/peeps", () => {
+    it("should respond with 200", (done) => {
+      request(app).get("/chitter/peeps").expect(200, done);
+    });
 
-  it("should respond with 200", (done) => {
-    request(app).post("/chitter").expect(200, done)
-  })
-})
+    it("should respond with 200", (done) => {
+      request(app).post("/chitter/peeps").expect(200, done);
+    });
+  });
+
+  describe("#/users", () => {
+    it("should respond with 200", (done) => {
+      request(app).get("/chitter/users").expect(200, done);
+    });
+
+    it("should respond with 200", (done) => {
+      request(app).post("/chitter/users").expect(200, done);
+    });
+  });
+});
